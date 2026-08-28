d6217e452d7cfe4f5f29285e54c396d96eff5293
Jesus Gonzalez <jesus.gonzlez.lopez@gmail.com>
2026-08-27 23:38:13 -0600

    Creacion de archivos factory para creacion de registros masivos

---
+++ database/factories/CursoFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Curso;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Curso>
+ */
+class CursoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre'=>$this->faker->sentence(),//oracion
+            'descripcion'=>$this->faker->paragraph(),//parrafo
+            'categoria'=>$this->faker->randomElement([
+                'Desarrollo Web',
+                'Diseño Web'
+            ])
+
+        ];
+    }
+}
